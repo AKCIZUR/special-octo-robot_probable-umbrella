b6c98e0fe48f90b229ff4c428622b63c441b23b9
AKCIZUR <ruzickajakub95@gmail.com>
2026-07-01 23:25:46 +0200

    feat: add theme.config.tsx for Nextra 4.6.1 production setup

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Docs</span>,
+  project: {
+    link: 'https://github.com/AKCIZUR/special-octo-robot_probable-umbrella',
+  },
+  chat: {
+    link: 'https://discord.gg',
+  },
+  docsRepositoryBase: 'https://github.com/AKCIZUR/special-octo-robot_probable-umbrella/tree/main',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
